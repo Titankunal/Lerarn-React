9a16969e27eb847409d69b1dac345bed666c9b28
Titankunal <89270242+Titankunal@users.noreply.github.com>
2025-06-04 21:49:38 +0530

    new file:   MyReactProject/src/assets/batman-arkham-3840x2160-18364.jpg 	modified:   MyReactProject/src/card.jsx

--- MyReactProject/src/card.jsx
+++ MyReactProject/src/card.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import img1 from './assets/batman-arkham-3840x2160-18364.jpg'
 
 const card = () => {
   return (
     <div className=''>
-        <img src="" alt="Image loading failed" />
+        <img src={img1} alt="Image loading failed" />
         <h2>Kunal Bisht</h2>
         <p>I am a programmer and I like to play Cricket</p>      
     </div>
